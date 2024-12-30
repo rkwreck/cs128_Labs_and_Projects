3b25819f38d398db46f2bf40a5428a038e54d4f4
Rini K <89210546+rkwreck@users.noreply.github.com>
2024-12-29 22:14:14 -0800

    Add files via upload

--- student-musical-trees-main/driver.cc
+++ student-musical-trees-main/driver.cc
@@ -7,7 +7,133 @@ using namespace std;
 
 int main() {
 
-  //TEST CASE 8
+  // MusicalTree test_tree(true);
+  // vector<Note> root_motif = {{50, 0.1}, {78, 0.7}, {84, 0.7}, {61, 0.4}, {67, 0.1}, {78, 0.1}};
+  // MotifNode* root = new MotifNode(root_motif);
+
+  // MusicalTree test_tree(true, root); 
+  // test_tree.PrintTree(root); 
+
+  vector<MotifNode*> selectedNodes; 
+
+  //make the root, 10
+  Note note_10 = Note(10, 10); 
+  Note note_11 = Note(10, 11); 
+  Note note_12 = Note(10, 12); 
+  vector<Note> root_motif; 
+  root_motif.push_back(note_10); 
+  root_motif.push_back(note_11); 
+  root_motif.push_back(note_12);
+  MotifNode* node_10 = new MotifNode(root_motif);
+  node_10->SetFitnessScore(10.0); 
+
+  selectedNodes.push_back(node_10); 
+
+  //add 10's children, <90, 80> 
+  Note note_90 = Note(10, 10);
+  Note note_91 = Note(90, 91); 
+  Note note_92 = Note(90, 92);     
+  vector<Note> motif_90;  
+  motif_90.push_back(note_90); 
+  motif_90.push_back(note_91); 
+  motif_90.push_back(note_92); 
+  MotifNode* node_90 = new MotifNode(motif_90);
+  node_90->SetFitnessScore(90.0); 
+
+  node_10->AddChildNode(node_90); 
+
+  selectedNodes.push_back(node_90); 
+
+
+  Note note_80 = Note(10, 10);    
+  vector<Note> motif_80;  
+  motif_80.push_back(note_80); 
+  MotifNode* node_80 = new MotifNode(motif_80);
+  node_80->SetFitnessScore(80.0); 
+
+  node_10->AddChildNode(node_80); 
+
+  selectedNodes.push_back(node_80); 
+
+
+  ////// MAKE THE TREE
+  MusicalTree test_tree(false, node_10, 3); 
+  test_tree.PrintTree(node_10);
+
+
+  vector<Note> all_motifs = test_tree.GenerateMelody(); 
+  cout << "these are all_motifs: " << endl; 
+  for (Note note : all_motifs){
+    cout << note.pitch << "-" << note.duration << " " << endl; 
+  }
+
+
+
+  //test_tree.SelectionPhase(); 
+  //test_tree.PrintTree(node_10);
+
+  // int size = 0;
+  // test_tree.CheckSize(node_10,size); 
+  // cout << "original size is: " << to_string(size) << endl; 
+
+  // test_tree.CheckInvariants();
+
+
+  //test_tree.ReproductionPhase(selectedNodes); 
+
+  //test_tree.PruneNodes(node_10, 9); 
+  //test_tree.PrintTree(node_10); 
+
+  // size = 0;
+  // test_tree.CheckSize(node_10,size); 
+  // cout << "new size is: " << to_string(size) << endl; 
+
+  // test_tree.CheckInvariants();
+  
+
+  //TEST SELECTION PHASE
+  // vector<MotifNode*> selectedNodes = test_tree.SelectionPhase(); 
+  // cout << "these are the selected nodes: " << endl; 
+  // for (MotifNode* node : selectedNodes){
+  //   cout << to_string(node->GetFitnessScore()) << endl; 
+  // }
+
+  // //TEST INVARIANT FUNCTION
+  
+
+  //cout << test_tree.CheckParent(node_10) << endl;  
+
+  //double maxFitnessScore = -1; 
+  //test_tree.FindMaxFitness(root, maxFitnessScore); 
+  //cout << "The max fitness in this tree is : " << to_string(maxFitnessScore) << endl; 
+
+
+
+  // test_tree.PruneNodes(node_10, 30);  //threshold 30 
+  // cout << "completed pruning" << endl; 
+  // test_tree.PrintTree(node_90); 
+  
+
+
+
+  // // Create MelodyGenerator object
+  // MusicalTree mt(true);
+  // //GenerateMP3(mt.GenerateMelody(), "starting.mp3");
+
+  // // Evolve the tree structure for a fixed number of generations
+  // mt.GeneticAlgorithm(150);
+
+  // // Generate melody by traversing the evolved tree structure
+  // std::vector<Note> melody = mt.GenerateMelody();
+
+  // // Generage MP3 based on melody vector
+  // //GenerateMP3(melody, "output.mp3");
+}
+
+
+/*
+
+//TEST CASE 8
 
  //make the root, 10
   Note note_10 = Note(10, 10); 
@@ -59,26 +185,17 @@ int main() {
   ////// MAKE THE TREE
   MusicalTree test_tree(true, node_10, 6); 
   test_tree.PrintTree(node_10); 
-  test_tree.PruneNodes(node_10, 30);  //threshold 30 
-  cout << "completed pruning" << endl; 
-  test_tree.PrintTree(node_90); 
-  
+  double maxFitnessScore = -1; 
+  test_tree.FindMaxFitness(node_10, maxFitnessScore); 
+  cout << "The max fitness in this tree is : " << to_string(maxFitnessScore) << endl; 
+
+
+*/
 
 
 
-  // // Create MelodyGenerator object
-  // MusicalTree mt(true);
-  // //GenerateMP3(mt.GenerateMelody(), "starting.mp3");
 
-  // // Evolve the tree structure for a fixed number of generations
-  // mt.GeneticAlgorithm(150);
 
-  // // Generate melody by traversing the evolved tree structure
-  // std::vector<Note> melody = mt.GenerateMelody();
-
-  // // Generage MP3 based on melody vector
-  // //GenerateMP3(melody, "output.mp3");
-}
 
 
 
